ac6ba5e72748d6e5b7fea0fe16b56057ceb38709
fluddshine <ilakkiyan.jeyakumar@gmail.com>
2018-06-21 14:43:05 -0700

    h bridge control code

---
+++ code/h-bridge-control/h-bridge-control.ino
@@ -0,0 +1,36 @@
+#include <phys253.h>
+#include <LiquidCrystal.h>
+
+const int motor_left = 0;
+const int motor_right = 1;
+
+void setup() {
+  #include <phys253setup.txt>
+
+  Serial.begin(9600);
+  LCD.clear();
+  LCD.setCursor(0,0);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  /** 
+   *              Hb1 - Hb2 
+   *  STOP        LOW - LOW
+   *  FORWARD     HIGH - LOW
+   *  REVERSE     LOW - HIGH
+   *  NOT ALLOWED HIGH - HIGH
+ */
+ 
+  float val = knob(6); 
+  float out = 0.5 * val - 255; 
+
+  motor.speed(motor_right, out);
+
+  LCD.setCursor(0, 0); 
+  LCD.print("Knob: ");
+  LCD.print(val); 
+  LCD.setCursor(0, 1); 
+  LCD.print("Speed: "); 
+  LCD.print(out); 
+}
