245b763906cebb3b66e038dda1ff428fca2ebc7f
fluddshine <ilakkiyan.jeyakumar@gmail.com>
2018-07-18 00:04:47 -0700

    PID Menu systeM

--- code/robot_control/src/main.cpp
+++ code/robot_control/src/main.cpp
@@ -1,6 +1,11 @@
 #include "includes.h"
 #pragma GCC diagnostic ignored "-Wwrite-strings"
 
+// Oled pins 
+constexpr int oled_scl = PB11; 
+constexpr int oled_sda = PB10; 
+OLED oled(oled_scl, oled_sda, 8); 
+
 // Motor pins 
 constexpr int right_motor_pin1 = PA0; 
 constexpr int right_motor_pin2 = PA1; 
@@ -34,10 +39,10 @@ int forwardDriveTime2 = 2000;
 
 // PID constants
 int qrdThreshold = 50; 
-int gain = 1;
-int p = 50; 
+int gain = 5;
+int p = 1; 
 int i = 0; 
-int d = 37; 
+int d = 4; 
 int pid_qrd_threshold = 200; 
 
 // Start state and speed
@@ -45,6 +50,7 @@ int motorStartState = 0;
 int defaultSpeed = 60; 
 TapeFollow pidControl = TapeFollow(left_pid_QRD, right_pid_QRD); 
 
+// Initialize motor
 Motor rightMotor = Motor(right_motor_pin1, right_motor_pin2); 
 Motor leftMotor = Motor(left_motor_pin1, left_motor_pin2); 
 MotorControl motorControl = MotorControl(motorStartState, defaultSpeed, leftMotor, rightMotor, 
@@ -59,11 +65,18 @@ int closeTime2 = 500;
 int lowerTime = 1000; 
 int resetTime = 500; 
 
+// Initialize arms and claws
 Arm rightArm = Arm(right_clamp_pin, right_arm_pin, right_push_button); 
 ClawSequence rightClaw = ClawSequence(rightArm, closeTime, raiseTime, openTime, closeTime2, lowerTime, resetTime); 
-
 Arm leftArm = Arm(left_clamp_pin, left_arm_pin, left_push_button); 
 ClawSequence leftClaw = ClawSequence(leftArm, closeTime, raiseTime, openTime, closeTime2, lowerTime, resetTime); 
+
+// Menu pins 
+bool start; 
+constexpr int startBtn = PC14;
+constexpr int menuPot = PB1; 
+constexpr int menuPlus = PA12; 
+constexpr int menuMinus = PA15; 
 
 void setup() { 
     Serial.begin(9600); 
@@ -75,10 +88,64 @@ void setup() {
     // Initialize claw buttons
     pinMode(right_push_button, INPUT_PULLDOWN); 
     pinMode(left_push_button, INPUT_PULLDOWN); 
+
+    // Menu dials 
+    pinMode(startBtn, INPUT_PULLDOWN);
+    start = digitalRead(startBtn);  
+    pinMode(menuPot, INPUT); 
+    pinMode(menuPlus, INPUT_PULLDOWN); 
+    pinMode(menuMinus, INPUT_PULLDOWN); 
+
+}
+
+int optionState = 0; 
+void pidMenu() { 
+    int potVal = analogRead(menuPot); 
+    oled.clrScr(); 
+    oled.print("PID Menu", 0, 0); 
+    oled.print("p: ", 0, 10); 
+    oled.print("d: ", 0, 20); 
+    oled.print("gain: ", 0, 30); 
+    oled.print("threshold: ", 0, 40); 
+
+    if (digitalRead(menuPlus)) {
+        if (optionState = 3) 
+            {optionState = 0;
+        }
+        else {
+            optionState++;
+        }
+    }
+    int potVal = analogRead(menuPot); 
+    oled.printNumI(potVal, RIGHT, (optionState+1)*10);
+    oled.print("<--", 10, (optionState+1)*10); 
+    switch (optionState) { 
+        case 0:
+            motorControl.updateP(potVal); 
+            break; 
+        case 1: 
+            motorControl.updateD(potVal); 
+            break; 
+        case 2: 
+            motorControl.updateGain(potVal);
+            break; 
+        case 3: 
+            motorControl.updateThreshold(potVal);
+            break; 
+    }
+
+    oled.update(); 
 }
 
 void loop() { 
-    motorControl.poll(); 
-    rightClaw.poll(); 
-    leftClaw.poll(); 
+    start = digitalRead(startBtn); 
+
+    if (!start) { 
+        pidMenu();  
+    }
+    else {
+        motorControl.poll(); 
+        rightClaw.poll(); 
+        leftClaw.poll(); 
+    }
 }
